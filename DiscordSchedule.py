e974ac727f3a96e0fca37b17b64e304f9a42f8e9
ethansocal <79533577+ethansocal@users.noreply.github.com>
2021-02-23 14:01:14 -0800

    Update DiscordSchedule.py

--- DiscordSchedule.py
+++ DiscordSchedule.py
@@ -1,7 +1,7 @@
 import discord
 
 
-TOKEN = 'ODEzODMyOTEyMDY3MzYyODI3.YDVC9g.8jeLDI3q0vehjJrMTz6sl4Q9qJU'
+TOKEN = ''
 client = discord.Client(intents=discord.Intents.all())
 mondaySchedule = "Period 0/5:9:45;Period 1/6:10:34;Break:11:14;Period 2/7:11:27;Period 3/8:12:13;Lunch:12:53;PAWS:13:34;Period 4/9:14:5;School's Over!:14:45"
 mondaySchedule = mondaySchedule.split(";")
