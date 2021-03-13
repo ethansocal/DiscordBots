35bc4e6ddad1bad23dd98332af7ccfa89420d3ff
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-03-13 11:57:55 -0800

    Added tweeting

--- PoliticsAndWar.py
+++ PoliticsAndWar.py
@@ -9,13 +9,31 @@ from discord.ext import tasks
 import json
 import typing
 import wolframalpha
+import tweepy
 
 load_dotenv()
 TOKEN = os.getenv("POLITICS_BOT_TOKEN")
-PoliticsAndWarToken = os.getenv("API_TOKEN")
-WolframAlphaToken = os.getenv("WOLFRAM_ALPHA_TOKEN")
-WolframAlphaClient = wolframalpha.Client("JX5AXE-TJWTE43T96")
+politicsAndWarToken = os.getenv("API_TOKEN")
+wolframAlphaToken = os.getenv("WOLFRAM_ALPHA_TOKEN")
+wolframAlphaClient = wolframalpha.Client(wolframAlphaToken)
 bot = commands.Bot(command_prefix="!")
+twitterAuthorization = tweepy.OAuthHandler("81VqLm1jR02lsVZrmmFULu30F", "ppSsfP1Uwu1QzoMmaa33K4sDgqaMGYAVzzwfBf5MnKU0SDMalH")
+twitterAuthorization.set_access_token("1312430026541867011-gzPwohIMeIPJ2KaPDbM5BA8t232MfX", "iCtfFWJqIKK3faTTyE8iVnLITON4GUH3c0J9IR0S9SmTH")
+twitterClient = tweepy.API(twitterAuthorization)
+try:
+    twitterClient.verify_credentials()
+    print("Twitter authentication success!")
+except:
+    print("Error during authentication.")
+
+def isAuthorized(ctx):
+    file = open("authorized_users.txt")
+    for userid in file.readlines():
+        if ctx.author.id == int(userid):
+            file.close()
+            return True
+    file.close()
+    return False
 
 @bot.event
 async def on_ready():
@@ -26,10 +44,38 @@ async def on_ready():
     #updateNations.start()
     updateResources.start()
 
-@bot.command(help="Get a smart answer from Wolfram Alpha. Get math help, quick answers, and AI answers.",brief="Get an answer from Wolfram Alpha.")
-async def answer(ctx, *, question : str):
-    data = WolframAlphaClient.query(question)
+@bot.event
+async def on_command_error(ctx, error):
+    embed = discord.Embed(title="Error", color=0xff0000)
+    if isinstance(error, commands.CheckFailure):
+        embed.add_field(name="Error Message", value="Missing Permissions!")
+    else:
+        embed.add_field(name="Error Message", value="Please try again")
+        print(error)
+    await ctx.send(embed=embed)
+
+@bot.command(help="Get a smart answer from Wolfram Alpha. Get math help, quick answers, and AI answers. You need to be authorized by Ethan to run this command.",brief="Get an answer from Wolfram Alpha.")
+@commands.check(isAuthorized)
+async def answer(ctx, *, question):
+    data = wolframAlphaClient.query(question)
     await ctx.message.reply(next(data.results).text)
+
+@bot.command(brief="Tweet in Ethan's Twitter account!", help="Tweet in Ethan's Twitter account! You need to be authorized by Ethan to run this command.")
+@commands.check(isAuthorized)
+async def tweet(ctx, *, text):
+    try:
+        twitterClient.update_status(text)
+        await ctx.message.reply("Tweet successfully sent!")
+    except tweepy.TweepError:
+        await ctx.message.reply("Error sending tweet.")
+
+@bot.command(brief="Authorize another user!", help="Authorize another user to get access to restricted commands! Only Ethan can run this command.")
+async def authorize(ctx, user : discord.Member):
+    if ctx.author.id == 710657087100944476:
+        file = open("authorized_users.txt", "a")
+        file.write("\n"+str(user.id))
+        file.close()
+        await ctx.message.reply("Successfully authorized "+user.name + "#"+user.discriminator+"!")
 
 @bot.command(help="Get the current trade price of a resource.", brief="Get the current trade prices.", aliases=["price","tradeprices"])
 async def tradeprice(ctx, resource="all"):
@@ -45,7 +91,7 @@ async def tradeprice(ctx, resource="all"):
     elif not resource.lower() in ['credits','steel','food','gasoline','oil','coal','munitions','uranium','iron', 'lead','bauxite','aluminum']:
         await ctx.message.reply("Incorrect resource. Please try again.")
         return
-    data = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource={resource}&key={PoliticsAndWarToken}")
+    data = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource={resource}&key={politicsAndWarToken}")
     data = data.json()
     embed = discord.Embed(title=data["resource"].capitalize(),color=0x03b1fc)
     embed.add_field(name="Average Price", value="```$"+data["avgprice"]+"```",inline=False)
@@ -56,7 +102,7 @@ async def tradeprice(ctx, resource="all"):
 @bot.command(help="Put a city ID or name to get the city/cities that correspond.", brief="Get a city by it's ID or name",aliases=["city"])
 async def getcity(ctx, *, nameOrID : typing.Union[int, str]):
     if type(nameOrID) == int:
-        data  = requests.get(f"http://politicsandwar.com/api/city/id={nameOrID}&key={PoliticsAndWarToken}")
+        data  = requests.get(f"http://politicsandwar.com/api/city/id={nameOrID}&key={politicsAndWarToken}")
         data = data.json()
         embed = discord.Embed(title="Get City Results", color=0x00ff00)
         for dataName in data:
@@ -68,7 +114,7 @@ async def getcity(ctx, *, nameOrID : typing.Union[int, str]):
     else:
         loading = await ctx.message.reply("Gathering cities, please wait <a:loading:747680523459231834> ...")
         cities = []
-        data  = requests.get(f"http://politicsandwar.com/api/all-cities/key={PoliticsAndWarToken}")
+        data  = requests.get(f"http://politicsandwar.com/api/all-cities/key={politicsAndWarToken}")
         data = data.json()
         for thing in data["all_cities"]:
             if thing["city_name"].lower() == nameOrID.lower():
@@ -80,7 +126,7 @@ async def getcity(ctx, *, nameOrID : typing.Union[int, str]):
         await loading.delete()
         if len(cities) == 1:
             await ctx.message.reply(str(len(cities))+ " city with the name "+nameOrID+" was found.")
-            data = requests.get(f'http://politicsandwar.com/api/city/id={cities[0].fields[1].value}&key={PoliticsAndWarToken}')
+            data = requests.get(f'http://politicsandwar.com/api/city/id={cities[0].fields[1].value}&key={politicsAndWarToken}')
             data = data.json()
             embed = discord.Embed(title="Get City Results", color=0xff0000)
             for dataName in data:
@@ -103,7 +149,7 @@ async def invite(ctx, *, leaderName="Ethan Henry"):
 @bot.command(help="Get a nation by its ID or name.",aliases=["nation","getnation"])
 async def who(ctx, *, nameOrID : typing.Union[int, str]):
     if type(nameOrID) == int:
-        data  = requests.get(f"http://politicsandwar.com/api/nation/id={nameOrID}&key={PoliticsAndWarToken}")
+        data  = requests.get(f"http://politicsandwar.com/api/nation/id={nameOrID}&key={politicsAndWarToken}")
         data = data.json()
         embed = discord.Embed(title="Get Nation Results", color=0x00ff00)
         if data["success"] != False:
@@ -131,7 +177,7 @@ async def who(ctx, *, nameOrID : typing.Union[int, str]):
                 return
             data1 = nation.split(":")
             if data1[0].lower() == nameOrID.lower():
-                data  = requests.get(f"http://politicsandwar.com/api/nation/id={data1[1]}&key={PoliticsAndWarToken}")
+                data  = requests.get(f"http://politicsandwar.com/api/nation/id={data1[1]}&key={politicsAndWarToken}")
                 data = data.json()
                 embed = discord.Embed(title="Get Nation Results", color=0x00ff00)
                 if data["success"] != False:
@@ -158,34 +204,36 @@ async def who(ctx, *, nameOrID : typing.Union[int, str]):
 @tasks.loop(hours=1)
 async def updateNations():
     try:
-        print("Updating nation cache...")
-        data = requests.get(f"https://politicsandwar.com/api/nations/?key={PoliticsAndWarToken}")
+        print("Updating nations cache...")
+        data = requests.get(f"https://politicsandwar.com/api/nations/?key={politicsAndWarToken}")
         data = data.json()
         file = open("nations_cache.txt", "w")
         for thing in data["nations"]:
             file.write(str(thing["nation"]) + ":"+str(thing["nationid"])+"\n")
-        print("Done")
+        print("Done updating nations cache.")
     except:
-        print("Error saving files")
+        print("Error updating nations cache.")
     finally:
        file.close()
 
 @tasks.loop(hours=1)
 async def updateResources():
     try:
-        print("Updating resource cache...")
+        print("Updating resources cache...")
         resourceInfo = dict()
         for resource in ['steel','credits','food','gasoline','oil','coal','munitions','uranium','iron', 'lead','bauxite','aluminum']:
-            data = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource={resource}&key={PoliticsAndWarToken}")
+            data = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource={resource}&key={politicsAndWarToken}")
             data = data.json()
             resourceInfo[resource] = int(data["avgprice"])
         file = open("resource_cache.json", "w")
         file.write(json.dumps(resourceInfo))
-        print("Done")
-    
+        print("Done updating resources cache.")
+    except:
+        print("Error updating resources cache.")
     finally:
         file.close()
-    
+
+
 
 
 bot.run(TOKEN)
